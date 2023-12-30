81f399f593a04275c1fc2beff393c50ad7613201
Hofbauer-Michael <hofbauer.michael@gmx.de>
2023-12-30 15:00:38 +0100

    added tests

---
+++ test.py
@@ -0,0 +1,26 @@
+
+import conftest
+import pytest
+
+
+class Test:
+    DATA_X = (
+        1,
+        1,
+        1,
+        1
+    )
+
+    DATA_Y = (
+        1,
+        2,
+        1,
+        1
+    )
+
+    @pytest.mark.sanity
+    @pytest.mark.usefixtures('print_fixture_use')
+    @pytest.mark.parametrize("x1_values", DATA_X)
+    @pytest.mark.parametrize("y1_values", DATA_Y)
+    def test_one(self, x1_values, y1_values):
+        assert x1_values == y1_values
