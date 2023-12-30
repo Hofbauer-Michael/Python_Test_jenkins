a0f92eb175d48071dc8e15b4645c294a076e7318
Hofbauer-Michael <hofbauer.michael@gmx.de>
2023-12-30 15:22:12 +0100

    added test branch

--- test.py
+++ test.py
@@ -32,3 +32,10 @@ class Test:
     def test_two(self, x1_values, y1_values):
         assert x1_values == y1_values
 
+    @pytest.mark.sanity
+    @pytest.mark.usefixtures('print_fixture_use')
+    @pytest.mark.parametrize("x1_values", DATA_X)
+    @pytest.mark.parametrize("y1_values", DATA_Y)
+    def test_three(self, x1_values, y1_values):
+        assert x1_values == y1_values
+
