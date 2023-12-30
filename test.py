b855f2d6462aee8057742aa9c7396b8aea071030
Hofbauer-Michael <hofbauer.michael@gmx.de>
2023-12-30 15:11:06 +0100

    added tests

--- test.py
+++ test.py
@@ -24,3 +24,11 @@ class Test:
     @pytest.mark.parametrize("y1_values", DATA_Y)
     def test_one(self, x1_values, y1_values):
         assert x1_values == y1_values
+
+    @pytest.mark.sanity
+    @pytest.mark.usefixtures('print_fixture_use')
+    @pytest.mark.parametrize("x1_values", DATA_X)
+    @pytest.mark.parametrize("y1_values", DATA_Y)
+    def test_two(self, x1_values, y1_values):
+        assert x1_values == y1_values
+
