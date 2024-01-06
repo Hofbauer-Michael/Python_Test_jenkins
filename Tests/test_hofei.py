53819d5f5bef48375532dccf6d674c10acf3ccbe
Hofbauer-Michael <hofbauer.michael@gmx.de>
2024-01-06 16:27:45 +0100

    added jenkinsfile

--- Tests/test_hofei.py
+++ Tests/test_hofei.py
@@ -2,7 +2,7 @@ import conftest
 import pytest
 
 
-class Test01:
+class Test:
     DATA_X = (
         1,
         1,
@@ -30,29 +30,4 @@ class Test01:
         assert y1_values == 1
 
 
-class Test02:
-    DATA_X = (
-        1,
-        1,
-        1,
-        1
-    )
 
-    DATA_Y = (
-        1,
-        1,
-        1,
-        1
-    )
-
-    @pytest.mark.sanity
-    @pytest.mark.usefixtures('print_fixture_use')
-    @pytest.mark.parametrize("x1_values", DATA_X)
-    def test_x2(self, x1_values):
-        assert x1_values == 1
-
-    @pytest.mark.sanity
-    @pytest.mark.usefixtures('print_fixture_use')
-    @pytest.mark.parametrize("y1_values", DATA_Y)
-    def test_y2(self, y1_values):
-        assert y1_values == 1
