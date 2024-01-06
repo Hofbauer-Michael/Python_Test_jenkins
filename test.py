aad28cebf92918beaeecd6f37ed0c748d03d9993
Hofbauer-Michael <hofbauer.michael@gmx.de>
2024-01-06 15:44:21 +0100

    added jenkinsfile

--- test.py
+++ test.py
@@ -37,10 +37,10 @@ class Test:
     @pytest.mark.usefixtures('print_fixture_use')
     @pytest.mark.parametrize("x1_values", DATA_X)
     def test_x2(self, x1_values):
-        assert x1_values == 2
+        assert x1_values == 1
 
     @pytest.mark.sanity
     @pytest.mark.usefixtures('print_fixture_use')
     @pytest.mark.parametrize("y1_values", DATA_Y)
     def test_y2(self, y1_values):
-        assert y1_values == 2
+        assert y1_values == 1
