4f325931244db51c5c2e3188fc7be2c1358ffd7e
Hofbauer-Michael <hofbauer.michael@gmx.de>
2024-01-06 15:30:15 +0100

    added jenkinsfile

--- test.py
+++ test.py
@@ -21,21 +21,26 @@ class Test:
     @pytest.mark.sanity
     @pytest.mark.usefixtures('print_fixture_use')
     @pytest.mark.parametrize("x1_values", DATA_X)
+    def test_x1(self, x1_values):
+        assert x1_values == 1
+
+
+
+    @pytest.mark.sanity
+    @pytest.mark.usefixtures('print_fixture_use')
     @pytest.mark.parametrize("y1_values", DATA_Y)
-    def test_one(self, x1_values, y1_values):
-        assert x1_values == y1_values
+    def test_y1(self, y1_values):
+        assert y1_values == 1
+
 
     @pytest.mark.sanity
     @pytest.mark.usefixtures('print_fixture_use')
     @pytest.mark.parametrize("x1_values", DATA_X)
-    @pytest.mark.parametrize("y1_values", DATA_Y)
-    def test_two(self, x1_values, y1_values):
-        assert x1_values == y1_values
+    def test_x2(self, x1_values):
+        assert x1_values == 2
 
     @pytest.mark.sanity
     @pytest.mark.usefixtures('print_fixture_use')
-    @pytest.mark.parametrize("x1_values", DATA_X)
     @pytest.mark.parametrize("y1_values", DATA_Y)
-    def test_three(self, x1_values, y1_values):
-        assert x1_values == y1_values
-
+    def test_y2(self, y1_values):
+        assert y1_values == 2
